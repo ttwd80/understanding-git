881b33ddd6f8dfbff8bc767e4a44f23416b62dd0
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-12-31 05:58:16 +1100

    Update functions.sh

--- bin/functions.sh
+++ bin/functions.sh
@@ -2,6 +2,7 @@ list-options-for-git-command() {
   git $1 --help | grep -- "^       -" | sed \
     -e 's/^       //g' \
     -e 's/, /\n/g'  \
+    -e 's/=/\n/g'  \
     -e 's/ /\n/g'  \
     -e 's/,$//g' \
     | grep -- "^--." | sort
