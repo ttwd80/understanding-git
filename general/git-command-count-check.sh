c2ae4979c4f86bd80579cdbfb066181de46e01c0
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-01-05 18:21:05 +1100

    using expected actual test

--- general/git-command-count-check.sh
+++ general/git-command-count-check.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-COUNT=$(curl --silent "https://raw.githubusercontent.com/git/git/master/command-list.txt" | grep "^git-" | wc -l | awk '{print $1}')
-test "${COUNT}" = "$(cat ${HOME}/build/ttwd80/understanding-git/general/git-command-count.txt)"
+GIT_COMMAND_COUNT_EXPECTED="$(cat ${HOME}/build/ttwd80/understanding-git/general/git-command-count.txt)"
+GIT_COMMAND_COUNT_ACTUAL="$(curl --silent "https://raw.githubusercontent.com/git/git/master/command-list.txt" | grep "^git-" | wc -l | awk '{print $1}')"
+test "${GIT_COMMAND_COUNT_EXPECTED}" = "${GIT_COMMAND_COUNT_ACTUAL}"
--- general/git-version-check.sh
+++ general/git-version-check.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-GIT_VERSION="$(cat ${HOME}/build/ttwd80/understanding-git/general/git-version.txt)"
-git --version | grep "${GIT_VERSION}$"
+GIT_VERSION_EXPECTED="$(cat ${HOME}/build/ttwd80/understanding-git/general/git-version.txt)"
+GIT_VERSION_ACTUAL="$(git --version | awk '{print $3}')"
+test "${GIT_VERSION_EXPECTED}" = "${GIT_VERSION_ACTUAL}"
