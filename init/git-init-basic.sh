fbb5e014e69854d08b245b38291ef94dc0fe871e
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-01-06 10:16:42 +1100

    basic use case for git

---
+++ init/git-init-basic.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+rm -rf ${HOME}/work
+mkdir -p ${HOME}/work
+cd ${HOME}/work
+
+#git status before git init
+if test git status
+then
+  echo "git status - ok"
+else
+  echo "git status - failed"
+fi
+
+if test -d .git
+then
+  echo ".git directory exists"
+else
+  echo ".git directory does not exist"
+fi
+
+echo "Running git init"
+git init
+
+if test git status
+then
+  echo "git status - ok"
+else
+  echo "git status - failed"
+fi
+
+if test -d .git
+then
+  echo ".git directory exists"
+else
+  echo ".git directory does not exist"
+fi
