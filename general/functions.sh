b86633e404551f154fbde5d6a7f973408f83bbc1
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-01-07 04:12:14 +1100

    Update functions.sh

--- general/functions.sh
+++ general/functions.sh
@@ -1,9 +1,3 @@
 extract_options() {
-  curl --silent "$1" |\
-    grep -E "OPT_STRING\(|OPT_BIT\(|OPT_SET_INT\(|OPTION_CALLBACK" |\
-    sed 's#{ OPTION_CALLBACK, #OPTION_CALLBACK(#' |\
-    awk '{ print $2 }' |\
-    sed -e 's/^"//' -e 's/\",//' |\
-    sort |\
-    awk '{print "--" $1}'
+  git $1 --help | grep "^\s*-" | grep -o -- "--.*" | sed -e 's/,.*//' -e 's/=.*//' | grep -v "^..$" | awk '{print $1}' | sort
 }
