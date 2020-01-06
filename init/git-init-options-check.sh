acd4ab669f2b48cdfe84d78f64117fffcefbf032
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-01-06 11:13:20 +1100

    Update git-init-options-check.sh

--- init/git-init-options-check.sh
+++ init/git-init-options-check.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+ls -l ${HOME}/build/ttwd80/understanding-git/general/functions.sh
 source ${HOME}/build/ttwd80/understanding-git/general/functions.sh
 diff <(extract_options "https://raw.githubusercontent.com/git/git/master/builtin/init-db.c") <(cat ${HOME}/build/ttwd80/understanding-git/init/git-init-options.txt)
