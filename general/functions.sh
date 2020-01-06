2683f03e11f27b4cb1cfaa72ae96b4524bee46fb
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-01-06 13:13:17 +1100

    breaking change

--- general/functions.sh
+++ general/functions.sh
@@ -1,6 +1,7 @@
 extract_options() {
   curl --silent "$1" |\
-    grep -E "OPT_STRING\(|OPT_BIT\(|OPT_SET_INT\(|OPTION_CALLBACK\(" |\
+    grep -E "OPT_STRING\(|OPT_BIT\(|OPT_SET_INT\(|OPTION_CALLBACK" |\
+    sed 's#{ OPTION_CALLBACK, #OPTION_CALLBACK(#' |\
     awk '{ print $2 }' |\
     sed -e 's/^"//' -e 's/\",//' |\
     sort |\
