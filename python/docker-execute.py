0e9d86c6ee8a2721360fb4c2436d49e719128433
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-02-16 04:21:10 +1100

    moved sys argv to the top and used varibles to better understand what the parameters are used for
    Signed-off-by: Titi Wangsa Damhore <ttwd80@gmail.com>

--- python/docker-execute.py
+++ python/docker-execute.py
@@ -1,14 +1,18 @@
 import pexpect
 import sys
 
+
+session_file = sys.argv[1]
+docker_image = sys.argv[2]
+command_to_run = sys.argv[3]
+
 content_array = []
-with open(sys.argv[1]) as f:
+with open(session_file) as f:
     for line in f:
         content_array.append(line)
 
-child = pexpect.spawn("docker run --rm -it {0} {1}".format(
-    sys.argv[2], sys.argv[3]),
-                      encoding='utf-8')
+child = pexpect.spawn(
+    "docker run --rm -it {0} {1}".format(docker_image, command_to_run), encoding='utf-8')
 child.logfile_read = sys.stdout
 child.setecho(False)
 
