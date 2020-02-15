0cbf28ec877b6539a1d7b51dc6811084e4152e8f
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-02-16 03:13:26 +1100

    direct docker call

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ pipenv clean && pipenv run pip install -q pexpect==4.8.0 Jinja2==2.11.1
 
 #Intro
 echo "Processing intro.md..."
-export TEMPLATE_MULTIPLE_SECTIONS="$(pipenv run python ./python/docker-execute.py ./python/intro.txt python 'su - backup -s /bin/sh' | grep -v '^\$')"
-export TEMPLATE_SINGLE_VERSION="$(pipenv run python ./python/docker-execute.py ./python/version.txt python 'su - backup -s /bin/sh' | grep -v '^\$' | awk '{print $3}')"
+export TEMPLATE_MULTIPLE_SECTIONS="$(docker run -it --rm python su - backup -s /bin/sh -c 'git help -a | cat' | grep -vE '>|<' | grep '^[A-Z].*')"
+export TEMPLATE_SINGLE_VERSION="$(docker run -it --rm python su - backup -s /bin/sh -c 'git --version' | cut -f 3 -d' ')"
 export TEMPLATE_HELP_CONTENT="$(pipenv run python ./python/docker-execute.py ./python/help.txt python 'su - backup -s /bin/sh' | grep -v '^\$')"
 pipenv run python \
     ./python/generate-markdown.py \
