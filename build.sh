6022e9cbd448d2eb4191fbb6e41fb349396c6252
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-02-16 18:27:33 +1100

    using README.md instead of index.md so that the markdown content is automatically displayed.

--- build.sh
+++ build.sh
@@ -16,91 +16,91 @@ pipenv run python \
 
 # Section 1
 # Main Porcelain Commands
-echo "Processing section-1/index.md..."
+echo "Processing section-1/README.md..."
 mkdir -p markdown/section-1
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     1 \
-    "Main Porcelain Commands" > markdown/section-1/index.md
+    "Main Porcelain Commands" > markdown/section-1/README.md
 
 # Section 2
 # Ancillary Commands / Manipulators
-echo "Processing section-2/index.md..."
+echo "Processing section-2/README.md..."
 mkdir -p markdown/section-2
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     2 \
-    "Ancillary Commands / Manipulators" > markdown/section-2/index.md
+    "Ancillary Commands / Manipulators" > markdown/section-2/README.md
 
 # Section 3
 # Ancillary Commands / Manipulators
-echo "Processing section-3/index.md..."
+echo "Processing section-3/README.md..."
 mkdir -p markdown/section-3
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     3 \
-    "Ancillary Commands / Interrogators" > markdown/section-3/index.md
+    "Ancillary Commands / Interrogators" > markdown/section-3/README.md
 
 # Section 4
 # Interacting with Others
-echo "Processing section-4/index.md..."
+echo "Processing section-4/README.md..."
 mkdir -p markdown/section-4
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     4 \
-    "Interacting with Others" > markdown/section-4/index.md
+    "Interacting with Others" > markdown/section-4/README.md
 
 # Section 5
 # Low-level Commands / Manipulators
-echo "Processing section-5/index.md..."
+echo "Processing section-5/README.md..."
 mkdir -p markdown/section-5
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     5 \
-    "Low-level Commands / Manipulators" > markdown/section-5/index.md
+    "Low-level Commands / Manipulators" > markdown/section-5/README.md
 
 # Section 6
 # Low-level Commands / Interrogators
-echo "Processing section-6/index.md..."
+echo "Processing section-6/README.md..."
 mkdir -p markdown/section-6
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     6 \
-    "Low-level Commands / Interrogators" > markdown/section-6/index.md
+    "Low-level Commands / Interrogators" > markdown/section-6/README.md
 
 # Section 7
 # Low-level Commands / Synching Repositories
-echo "Processing section-7/index.md..."
+echo "Processing section-7/README.md..."
 mkdir -p markdown/section-7
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     7 \
-    "Low-level Commands / Synching Repositories" > markdown/section-7/index.md
+    "Low-level Commands / Synching Repositories" > markdown/section-7/README.md
 
 # Section 8
 # Low-level Commands / Internal Helpers
-echo "Processing section-8/index.md..."
+echo "Processing section-8/README.md..."
 mkdir -p markdown/section-8
 pipenv run python \
     ./python/generate-section.py \
     section-index.md.jinja \
     TEMPLATE_HELP_CONTENT \
     8 \
-    "Low-level Commands / Internal Helpers" > markdown/section-8/index.md
+    "Low-level Commands / Internal Helpers" > markdown/section-8/README.md
 
 # Section 1/8, Command 21/39
 # git init
