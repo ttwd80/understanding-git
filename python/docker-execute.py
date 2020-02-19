6f35e3dee2ae1570b531aea8ca03ee8ac5a6e95e
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-02-19 15:56:44 +1100

    simple command file

--- python/docker-execute.py
+++ python/docker-execute.py
@@ -15,14 +15,10 @@ child = pexpect.spawn(
     "docker run --rm -it {0} {1}".format(docker_image, command_to_run), encoding='utf-8')
 child.logfile_read = sys.stdout
 child.setecho(False)
+child.expect("<\\$ $")
 
 for line in content_array:
     line = line.rstrip("\n")
-    if len(line) > 1:
-        c = line[0]
-        if (c == '<'):
-            child.expect(line[1:])
-        elif (c == '>'):
-            child.sendline(line[1:])
-        else:
-            print("Unknown command: {0}".format(c))
+    if ((len(line) > 0) and (line[0] != "#")):
+        child.sendline(line)
+        child.expect("<\\$ $")
