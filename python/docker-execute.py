43c2d4b056d49b8c66eefbeb8471f56705555bf5
Titi Wangsa Damhore <ttwd80@gmail.com>
2020-03-06 20:30:49 +1100

    using one character to switch between root and normal user

--- python/docker-execute.py
+++ python/docker-execute.py
@@ -11,14 +11,24 @@ with open(session_file) as f:
     for line in f:
         content_array.append(line)
 
+expect_prompt = "\\$ $"
 child = pexpect.spawn(
     "docker run --rm -it {0} {1}".format(docker_image, command_to_run), encoding='utf-8')
 child.logfile_read = sys.stdout
 child.setecho(False)
-child.expect("\\$ $")
+child.expect(expect_prompt)
 
 for line in content_array:
     line = line.rstrip("\n")
-    if ((len(line) > 0) and (line[0] != "#")):
+    if len(line) == 1:
+        if line == "$":
+            expect_prompt = "\\$ $"
+        elif line == "#":
+            expect_prompt = "# $"
+        else:
+            sys.stderr.write("Unknown command [" + line + "]")
+            exit(-1)
+
+    if ((len(line) > 1) and (line[0] != "#")):
         child.sendline(line)
-        child.expect("\\$ $")
+        child.expect(expect_prompt)
